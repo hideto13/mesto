daee520b138667743a415f4db4504fd862b68195
hideto13 <pinki_00@mail.ru>
2021-10-20 23:28:04 +0300

    Change styles and scripts

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,23 @@ const popupElement = document.querySelector('.popup')
 const popupCloseButtonElement = popupElement.querySelector('.popup__close')
 const profileTitleElement = document.querySelector('.profile__title')
 const profileSubtitleElement = document.querySelector('.profile__subtitle')
-const popupNameInputElement = popupElement.querySelector('.popup__name-input')
-const popupTextInputElement = popupElement.querySelector('.popup__text-input')
-const popupSubmitButtonElement = popupElement.querySelector('.popup__submit-button')
+const popupNameInputElement = popupElement.querySelector('.popup__input_field_name')
+const popupTextInputElement = popupElement.querySelector('.popup__input_field_text')
+const popupFormElement = popupElement.querySelector('.popup__form')
 
-const togglePopupVisibility = function() {
-  popupElement.classList.toggle('popup_opened')
+const openPopup = function() {
+  popupNameInputElement.value = profileTitleElement.textContent
+  popupTextInputElement.value = profileSubtitleElement.textContent
+  popupElement.classList.add('popup_opened')
+}
+
+const closePopup = function() {
+  popupElement.classList.remove('popup_opened')
 }
 
 const closePopupByClickOnOverlay = function(event) {
   if (event.target === event.currentTarget) {
-    togglePopupVisibility()
+    closePopup()
   }
 }
 
@@ -21,10 +27,10 @@ const formSubmitHandler = function(evt) {
   evt.preventDefault();
   profileTitleElement.textContent = popupNameInputElement.value
   profileSubtitleElement.textContent = popupTextInputElement.value
-  togglePopupVisibility()
+  closePopup()
 }
 
-popupOpenButtonElement.addEventListener('click', togglePopupVisibility)
-popupCloseButtonElement.addEventListener('click', togglePopupVisibility)
+popupOpenButtonElement.addEventListener('click', openPopup)
+popupCloseButtonElement.addEventListener('click', closePopup)
 popupElement.addEventListener('click', closePopupByClickOnOverlay)
-popupSubmitButtonElement.addEventListener('click', formSubmitHandler)
+popupFormElement.addEventListener('submit', formSubmitHandler)
