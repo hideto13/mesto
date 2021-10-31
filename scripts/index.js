15d8228cad8b01b04f089d34df292ec9d4382b8f
hideto13 <pinki_00@mail.ru>
2021-10-31 23:34:04 +0300

    Refactor initial cards generating

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+const cardsList = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('.card_template');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button')
 const popupElement = document.querySelector('.popup')
 const popupCloseButtonElement = popupElement.querySelector('.popup__close')
@@ -6,6 +34,29 @@ const profileSubtitleElement = document.querySelector('.profile__subtitle')
 const popupNameInputElement = popupElement.querySelector('.popup__input_field_name')
 const popupTextInputElement = popupElement.querySelector('.popup__input_field_text')
 const popupFormElement = popupElement.querySelector('.popup__form')
+
+function main() {
+	initialCards.forEach((element) => {
+		renderCard(element.name, element.link);
+	})
+
+	popupOpenButtonElement.addEventListener('click', openPopup)
+  popupCloseButtonElement.addEventListener('click', closePopup)
+  popupElement.addEventListener('click', closePopupByClickOnOverlay)
+  popupFormElement.addEventListener('submit', formSubmitHandler)
+}
+
+function renderCard(name, link) {
+
+	const htmlElement = cardTemplate.content.cloneNode(true);
+
+	htmlElement.querySelector('.card__text').innerText = name;
+  htmlElement.querySelector('.card__image').src = link;
+  htmlElement.querySelector('.card__image').alt = name;
+
+	cardsList.appendChild(htmlElement);
+
+}
 
 const openPopup = function() {
   popupNameInputElement.value = profileTitleElement.textContent
@@ -30,7 +81,4 @@ const formSubmitHandler = function(evt) {
   closePopup()
 }
 
-popupOpenButtonElement.addEventListener('click', openPopup)
-popupCloseButtonElement.addEventListener('click', closePopup)
-popupElement.addEventListener('click', closePopupByClickOnOverlay)
-popupFormElement.addEventListener('submit', formSubmitHandler)
+main();
